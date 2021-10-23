eb070bd4496d01f42c0a1e860613a0a96df7d2d2
Kevin Cao <k1cao@ucsd.edu>
2021-10-23 13:48:35 -0700

    second commit

---
+++ src/components/ButtonAppBar.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            LOGO
+          </Typography>
+          <Button color="inherit">Questionnaire</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
